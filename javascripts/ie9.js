3594a6b170e7ac6c4e230a3ee72e3c49b1814e91
martent <marten@thavenius.se>
2014-11-13 11:25:54 +0100

    Snake refactoring

--- javascripts/ie9.js
+++ javascripts/ie9.js
@@ -1,3 +1,3 @@
 jQuery(function($) {
-  $(".nav-page-snake").before("<div class='warning'> <p>Du verkar använda IE9 eller äldre. Därför visas inte navigationen nedan i kolumner. Denna lösning kommer inte att släppas förrän vi har uppgraderat till en modern webbläsare.</p>");
+  $(".snake").before("<div class='warning'> <p>Du verkar använda IE9 eller äldre. Därför visas inte navigationen nedan i kolumner. Denna lösning kommer inte att släppas förrän vi har uppgraderat till en modern webbläsare.</p>");
 });
